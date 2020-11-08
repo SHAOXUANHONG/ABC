286100b94a12d39f033eada373a2df7402a645b2
SHAOX HONG <asd0000fg@gmail.com>
2020-11-08 14:37:40 +0800

    1version

---
+++ CalTest.java
@@ -0,0 +1,30 @@
+package HomeWork6_1;
+
+import java.util.Scanner;
+
+import javax.swing.text.html.HTMLEditorKit.InsertHTMLTextAction;
+
+public class CalTest {
+	public static void main(String[] args) {
+		Calculator number = new Calculator();
+
+		while (true) {
+			try {
+				Scanner input = new Scanner(System.in);
+				System.out.println("請輸入x的值");
+				number.setX(input.nextInt());
+				System.out.println("請輸入y的值");
+				number.setY(input.nextInt());
+				double sum = number.getSquare();
+				System.out.println(number.getX() + "的" + number.getY() + "次方,答案為=" + sum);
+				break;
+			} catch (CallException e) {
+				System.out.println(e.toString());
+				System.out.println("請重新輸入");
+			} catch (Exception e) {
+				System.out.println("輸入格式錯誤");
+				System.out.println("請重新輸入");
+			}
+		}
+	}
+}
